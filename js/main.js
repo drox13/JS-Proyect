a5569023e27c04413893ddd41a71e09a67e6238f
drox13 <dario.baron@uptc.edu.co>
2023-03-01 00:00:40 -0500

    se hizo un login falso usando localStorage

--- js/main.js
+++ js/main.js
@@ -72,7 +72,29 @@ $(document).ready(function(){
     $("html, body").animate({
         scrollTop: 0,
     }, 500)
+    return false;
    })
-   return false;
 
+   //formulario LOGIN FALSO
+   $("#login form").submit(function(){
+    var form_name = $("#form_name").val()
+
+    localStorage.setItem("form_name", form_name)
+   })
+
+   var form_name = localStorage.getItem("form_name")
+   if(form_name != null && form_name != "undefined"){
+        var about_parrafo = $("#about p")
+
+        about_parrafo.html("<br><strong> Bienvenido, " +form_name +"</strong>")
+        about_parrafo.append("<a href='#' id='logout'> Cerrar sesion </a>")
+        
+        $("#login").hide()
+
+        $("#logout").click(function(){
+            localStorage.removeItem("form_name");
+            location.reload()
+        })
+   }
+ 
 });
