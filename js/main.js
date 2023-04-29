0c5702e26fd2b3fcc97d2d4c7cebb37bcff8adc0
drox13 <dario.baron@uptc.edu.co>
2023-04-29 12:03:46 -0500

    se integro un pluggin para validar el formulario end porject

--- js/main.js
+++ js/main.js
@@ -113,4 +113,18 @@ $(document).ready(function(){
     },1000)
    }
  
+   //cargando el validador del formulario
+   if(window.location.href.indexOf("contact") > -1){
+    
+    $("form input[name='date']").datepicker({
+        dateFormat: "dd-mm-yy"
+    });
+    
+    $.validate({
+        lang: "es",
+        errorMessagePosition: "top",
+        scrollToTopOnError: true
+    });
+   }
+ 
 });
